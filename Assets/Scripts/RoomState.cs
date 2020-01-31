e957f5f67df2b5855d1afa9464dc91cec1adcb00
Olli Etuaho <olli.etuaho@gmail.com>
2020-01-31 21:51:18 +0200

    Some preliminary framework code

---
+++ Assets/Scripts/RoomState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomState
+{
+
+    public enum FlowerStateType
+    {
+        Alive,
+        Dry
+    }
+
+    private FlowerStateType FlowerState;
+
+    public void ApplyState(Room room)
+    {
+        if (FlowerState == FlowerStateType.Alive)
+        {
+            // Room.Flower.SetState(FlowerStateType.Alive);
+        }
+        else
+        {
+            // Room.Flower.SetState(FlowerStateType.Dry);
+        }
+    }
+
+    public RoomState GenerateNextState()
+    {
+        // TODO: This should generate the state for the next day
+        return null;
+    }
+    
+}
