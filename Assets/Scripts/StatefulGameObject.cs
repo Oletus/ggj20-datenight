aeb8dff5d79d7fcf1d2f679806b4b6a74fd89901
Olli Etuaho <olli.etuaho@gmail.com>
2020-02-01 13:06:29 +0200

    State definitions can have null active objects or same active object as another state

--- Assets/Scripts/StatefulGameObject.cs
+++ Assets/Scripts/StatefulGameObject.cs
@@ -1,14 +1,17 @@
 ﻿using NaughtyAttributes;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 #pragma warning disable 0649
 
 [System.Serializable]
-public class StateObjectPair
+public class StateDefinition
 {
     [SerializeField] public string State; // generic T didnt work with Unity editing -_- ....
-    [SerializeField] public GameObject Object;
+
+    [FormerlySerializedAs("Object")]
+    [SerializeField] public GameObject ActiveObject;
 }
 
 public class StatefulGameObject : MonoBehaviour
@@ -16,21 +19,36 @@ public class StatefulGameObject : MonoBehaviour
     [SerializeField] private string Id;
 
     [ReorderableList]
-    [SerializeField] private List<StateObjectPair> States;
+    [SerializeField] private List<StateDefinition> States;
 
     [ReorderableList]
     [SerializeField] private List<string> IdsThisCanBeUsedOn;
 
     public void SetState<T>(T activeState) where T : System.Enum
     {
-        foreach(StateObjectPair pair in States)
+        GameObject activeObject = null;
+        HashSet<GameObject> activeObjectsFromAllStates = new HashSet<GameObject>();
+        foreach(StateDefinition state in States)
         {
-            if (pair.Object == null)
+            if ( state.State.ToLower().Trim() == activeState.ToString().ToLower() )
             {
-                Debug.LogError("null object in StatefulGameObject");
-                continue;
+                activeObject = state.ActiveObject;
             }
-            pair.Object.SetActive(pair.State.ToLower().Trim() == activeState.ToString().ToLower());
+            if ( state.ActiveObject != null )
+            {
+                activeObjectsFromAllStates.Add(state.ActiveObject);
+            }
+        }
+        if ( activeObject != null )
+        {
+            activeObject.SetActive(true);
+        }
+        foreach (GameObject obj in activeObjectsFromAllStates)
+        {
+            if (obj != activeObject)
+            {
+                obj.SetActive(false);
+            }
         }
     }
 }
