1a25b7441f565d25ec8236312b20cdcfbf4fbad1
Felor <antti.hamara@gmail.com>
2020-02-01 14:10:53 +0200

    code clean up

--- Assets/Scripts/CameraEngine.cs
+++ Assets/Scripts/CameraEngine.cs
@@ -7,19 +7,6 @@ public class CameraEngine : MonoBehaviour
     public GameObject MainCam;
     public GameObject[] RoomCam;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void SelectRoom(int roomNumber)
     {
         MainCam.transform.position = RoomCam[roomNumber].transform.position;
