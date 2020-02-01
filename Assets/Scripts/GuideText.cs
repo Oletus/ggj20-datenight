ad001201e8ad433fc5c3267dee3861a5b3c67ee7
Jaakko Lipsanen <jaakko.lipsanen@reaktor.fi>
2020-02-01 15:11:25 +0200

    Add basic guidetext

---
+++ Assets/Scripts/GuideText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuideText : MonoBehaviour
+{
+    private float _timeSinceLastSetText = 10000;
+    public Text Text { get { return this.GetComponent<Text>(); } }
+
+    private void Awake()
+    {
+        this.Text.CrossFadeAlpha(0, 0.5f, true);
+    }
+
+    public void SetText(string text)
+    {
+        this.Text.CrossFadeAlpha(1, 0.5f, true);
+        _timeSinceLastSetText = 0;
+        this.Text.text = text;
+
+    }
+
+    private void Update()
+    {
+        const float TimeToShowText = 5;
+        if(_timeSinceLastSetText < TimeToShowText &&  _timeSinceLastSetText + Time.deltaTime > TimeToShowText)
+        {
+            this.Text.CrossFadeAlpha(0, 0.5f, true);
+        }
+
+        _timeSinceLastSetText += Time.deltaTime;
+    }
+}
