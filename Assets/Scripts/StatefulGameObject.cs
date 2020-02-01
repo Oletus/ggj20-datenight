7d497b0a1532ad33086726f6a6c3e246fd45b736
Olli Etuaho <olli.etuaho@gmail.com>
2020-02-01 12:51:25 +0200

    Add id to StatefulGameObject

--- Assets/Scripts/StatefulGameObject.cs
+++ Assets/Scripts/StatefulGameObject.cs
@@ -1,23 +1,34 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+#pragma warning disable 0649
+
 [System.Serializable]
 public class StateObjectPair
 {
     [SerializeField] public string State; // generic T didnt work with Unity editing -_- ....
-    [SerializeField] public GameObject Object; // TODO: do we actually want this to be a list?
+    [SerializeField] public GameObject Object;
 }
 
 public class StatefulGameObject : MonoBehaviour
 {
+    [SerializeField] private string Id;
+
     // TODO: Add NaughtyAttributes library
     //[ReorderableList]
-    [SerializeField] public List<StateObjectPair> States;
+    [SerializeField] private List<StateObjectPair> States;
+
+    [SerializeField] private List<string> IdsThisCanBeUsedOn;
 
     public void SetState<T>(T activeState) where T : System.Enum
     {
         foreach(StateObjectPair pair in States)
         {
+            if (pair.Object == null)
+            {
+                Debug.LogError("null object in StatefulGameObject");
+                continue;
+            }
             pair.Object.SetActive(pair.State.ToLower().Trim() == activeState.ToString().ToLower());
         }
     }
